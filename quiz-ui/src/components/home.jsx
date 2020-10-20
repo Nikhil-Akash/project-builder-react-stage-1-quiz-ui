c93a74c63cfac312761612c6b601e875ff91a463
Nikhil Akash <samineninikhilakash@gmail.com>
2020-10-20 20:30:08 +0530

    quiz-ui-static

---
+++ quiz-ui/src/components/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Home() {
+    return(
+    <div className="homepage">
+        <h1>Quiz App</h1>
+        <button className="playbtn">play</button>
+    </div>
+    );
+}
+
